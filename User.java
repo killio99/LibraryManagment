535c71de4f92bf805937ba37fb7607b3bb6a8997
Ray <orozcoray.0202@gmail.com>
2025-04-06 23:29:35 -0400

    added reserveBook and payFines method to User class. Also added some getters and a setFines method

--- User.java
+++ User.java
@@ -21,7 +21,7 @@ public class User{
         fines = 0;
     }
 
-    /* 
+
     public void checkout(Book b){
         if (fines > 0){
             System.out.println("You have overdue fines. Please pay them before borrowing books.");
@@ -46,5 +46,48 @@ public class User{
         }else{
             System.out.println("This book was not borrowed by the user.");
         }
+    }
+
+    public void reserveBook(Book b){
+        if(!reservedBooks.contains(b)){
+            reservedBooks.add(b);
+            System.out.println("Reserved: " + b.getTitle());
+        }else {
+            System.out.println("Already reserved this book.");
+        }
+    }
+
+    public void payFines(double amount){
+        if(amount <= 0){
+            System.out.println("Invalid payment amount.");
+        }
+
+        fines -= amount;
+        if (fines < 0){
+            fines = 0;
+        }
+        System.out.println("Paid fines. REmianing balance: $" + fines);
+    }
+
+    //Getters
+    public String getUsername(){
+        return username;
+    }
+
+    public double getFines(){
+        return fines;
+    }
+
+    public ArrayList<Book> getBorrowedBooks(){
+        return borrowedBooks;
+    }
+
+    public ArrayList<Book> getReservedBooks(){
+        return reservedBooks;
+    }
+
+    //set fines
+    public void setFines(double amount){
+        fines = amount;
     }
 }
