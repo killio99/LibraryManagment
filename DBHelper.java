4b4f057971264d76eb81c82c01f29ade85e78580
Ray <orozcoray.0202@gmail.com>
2025-04-30 10:19:40 -0400

    fixed url

--- DBHelper.java
+++ DBHelper.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DBHelper {
     private static final String URL = 
-    "jdbc:sqlite:c:/Users/killi/OneDrive/Documents/Library Project/library.db";
+    "jdbc:sqlite:/Users/rayorozco/Documents/GitHub/LibraryManagment/library.db";
     
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL);
