4a035ed2edc841c00084af208765a71081533feb
Ray <orozcoray.0202@gmail.com>
2025-04-30 10:41:13 -0400

    added relative path

--- DBHelper.java
+++ DBHelper.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DBHelper {
     private static final String URL = 
-    "jdbc:sqlite:/Users/rayorozco/Documents/GitHub/LibraryManagment/library.db";
+    "jdbc:sqlite:library.db";
     
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL);
